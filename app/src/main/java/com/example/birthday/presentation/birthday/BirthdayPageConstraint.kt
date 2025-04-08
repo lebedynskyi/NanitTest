4f495c71a0aa568853981c03a5a879d96156e4e1
Vitalii Lebedynskyi <vetal.lebed@gmail.com>
2025-04-08 18:13:20 +0200

    Some code cleanup

--- app/src/main/java/com/example/birthday/presentation/birthday/BirthdayPageConstraint.kt
+++ app/src/main/java/com/example/birthday/presentation/birthday/BirthdayPageConstraint.kt
@@ -1,18 +1,7 @@
 package com.example.birthday.presentation.birthday
 
 import android.app.Activity
-import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Picture
-import android.graphics.Rect
 import android.net.Uri
-import android.os.Environment
-import android.os.Handler
-import android.os.Looper
-import android.text.format.DateFormat
-import android.view.PixelCopy
-import android.view.View
-import android.view.Window
 import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -44,16 +33,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asAndroidBitmap
-import androidx.compose.ui.graphics.drawscope.draw
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.layer.drawLayer
-import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.graphics.rememberGraphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
@@ -76,9 +60,6 @@ import com.example.birthday.presentation.base.theme.AppTheme
 import com.example.birthday.presentation.base.theme.BirthdayTheme
 import com.example.birthday.presentation.base.theme.LocalTheme
 import java.io.File
-import java.io.FileOutputStream
-import java.util.Date
-import androidx.core.graphics.createBitmap
 import kotlinx.coroutines.launch
 
 private val IMAGE_NUMBERS = buildMap {
